a968508d52193a86dd236552d83d014497bb0c68
Ilya_Morozkin <morozkin.ilya86@mail.ru>
2022-02-27 12:15:42 +0300

    task42 1

--- task42/Program.cs
+++ task42/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Определить, сколько чисел больше 0 введено с клавиатуры.
+
+string getNumberStr = String.Empty;
+int getNumber = 1;
+int i = 0;
+Console.WriteLine("Введите число, для выхода нажать 0 ");
+while (getNumber != 0 && getNumber > 0)
+{
+    getNumberStr = Console.ReadLine();
+    int.TryParse(getNumberStr, out getNumber);
+    i++;
+    if(getNumber == 0 || getNumber < 0)
+    i--;
+}
+Console.WriteLine($"Вы ввели {i} числа");
